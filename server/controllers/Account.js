8e0ae84ac91c136531eeed6f790f801d945ad0e4
Wyatt Lynch (RIT Student) <wal8273@ad.rit.edu>
2019-11-06 14:48:54 -0500

    fixed and finished everything

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -5,9 +5,7 @@ const loginPage = (req,res) =>{
     res.render('login', {csrfToken: req.csrfToken()});
 };
 
-const signupPage = (req,res) =>{
-    res.render('signup', {csrfToken: req.csrfToken()});
-};
+
 
 const logout = (req,res) =>{
     req.session.destroy();
@@ -72,12 +70,24 @@ const signup = (request, response) =>{
             }
             return res.status(400).json({error: 'An error occurred'});
         })
-    })
+    });
 
+   
+
+};
+
+const getToken = (request, response) =>{
+    const req = request;
+    const res = response;
+
+    const csrfJSON = {
+        csrfToken: req.csrfToken(),
+    };
+    res.json(csrfJSON);
 };
 
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
+module.exports.getToken = getToken;
 module.exports.signup = signup;
